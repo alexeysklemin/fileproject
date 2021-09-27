8cd15f648d92491c41029b88a4053b87376106b9
нет2 каби <a_sklemin@mail.ru>
2021-09-27 18:41:18 +0400

    Добавьте файлы проекта.

---
+++ fileproject/fileproject.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+struct friends {
+    short id;
+    std::string friendName;
+    
+    };
+    struct phones {
+        short id;
+        int idFriend;
+        std::string phone;
+
+
+    };
+
+int main(){
+    std::ifstream file;
+    if (!file.is_open()) {
+        file.open("friends.txt", std::ios::out);
+        file.open("phones.txt", std::ios::out);
+    }
+    
+
+    
+}
