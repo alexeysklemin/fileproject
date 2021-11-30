f43dbe5f4d486fdeb3498703e3c10e3456d417c0
нет2 каби <a_sklemin@mail.ru>
2021-11-30 20:42:13 +0400

    new ver 30.11,2021 new Structures and methods

--- fileproject/fileproject.cpp
+++ fileproject/fileproject.cpp
@@ -1,46 +1,87 @@
-﻿#include <iostream>
+﻿//#include <iostream>
+//#include <fstream>
+//#include <string>
+//
+//struct friends {
+//    int id;
+//    std::string friendName;
+//    
+//    };
+//    struct phones {
+//        int id;
+//        int idFriend;
+//        std::string phone;
+//
+//
+//    };
+//    void fileWork(std::string path) {
+//        if (true) {
+//            std::fstream fs;
+//            fs.open(path, std::fstream::in | std::fstream::out | std::fstream::app);
+//            if (!fs.is_open()) {
+//                std::ifstream(path);
+//
+//            }
+//    }
+//}
+//
+//    void writToFile() {
+//        fs.write((char*)&friends, sizeof(friends));
+//    }
+//   
+//
+//int main(){
+//  std::string path = "friendName.txt";
+//  fileWork(path);
+//  path = "phones.txt";
+//  fileWork(path);
+//  std::string Name;
+//  friends f;
+//  f.id = 1;
+//  f.friendName = "Vanya";
+//
+//    
+//
+//    
+//}
+#include <iostream>
 #include <fstream>
 #include <string>
 
 struct friends {
-    int id;
-    std::string friendName;
-    
-    };
-    struct phones {
-        int id;
-        int idFriend;
-        std::string phone;
+	    int id;
+	    std::string friendName;
+		void inp() {
+			std::cin >> id >> friendName;
+		}
+	  };
+	
+struct phones {
+	        int id;
+	        int idFriend;
+	        std::string phone;
+			void inp() {
+				std::cin >> id >> phone;
+			}
+	
+	    };
 
-
-    };
-    void fileWork(std::string path) {
-        if (true) {
-            std::fstream fs;
-            fs.open(path, std::fstream::in | std::fstream::out | std::fstream::app);
-            if (!fs.is_open()) {
-                std::ifstream(path);
-
-            }
-    }
+void writeToFile(std::string pathFile){
+	
 }
 
-    void writToFile() {
-        fs.write((char*)&friends, sizeof(friends));
-    }
-   
+void readFrFile(std::string pathFile) {
 
-int main(){
-  std::string path = "friendName.txt";
-  fileWork(path);
-  path = "phones.txt";
-  fileWork(path);
-  std::string Name;
-  friends f;
-  f.id = 1;
-  f.friendName = "Vanya";
+}
 
-    
+void delStringofFile() {
 
-    
+}
+
+int main() {
+	friends f;
+	phones tel;
+	f.id = 1;
+	tel.id = f.id;
+
 }
