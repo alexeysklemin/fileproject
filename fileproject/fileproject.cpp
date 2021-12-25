7b51ea8f9e367114bbbbc4c2c76097f065fb2bf5
нет2 каби <a_sklemin@mail.ru>
2021-12-25 23:49:31 +0400

    unliked version

--- fileproject/fileproject.cpp
+++ fileproject/fileproject.cpp
@@ -3,7 +3,7 @@
 #include <string>
 
 struct friends {
-	    std::string id;
+	    int id;
 	    std::string friendName;
 		void printFriend() {
 			std::cout << id << "\t" << friendName << "\t" << std::endl;
@@ -16,28 +16,45 @@ struct phones {
 	        int id = 0;
 	        int idFriend = 0;
 	        std::string phone;
+			void printPhones() {
+			std::cout << id << "\t" << phone << "\t" << std::endl;
+			}
 	
 };
 
-void writeToFile(std::string pathFile, friends, phones){
+void inputDatas() {
+	friends f;
+	phones p;
+	
+	std::cin >> f.id >> f.friendName >> p.id >> p.phone;
+	
+	p.idFriend = f.id;
+
+}
+
+void writeToFile(std::string pathFile, friends, phones) {
 	std::ofstream Fout;
 	friends f;
 	phones p;
-	std::cin >> f.id >> f.friendName;
-	//>> p.id >> p.phone;
-	f.id = p.idFriend;
 	Fout.open(pathFile);
-	Fout.write((char*)&f, sizeof(phones));
+	Fout.write((char*)&f, sizeof(friends));
 	Fout.close();
+	Fout.open(pathFile);
+	Fout.write((char*)&p, sizeof(phones));
 }
 
 void readFrFile(std::string pathFile, friends, phones) {
 	std::ifstream Fin;
 	friends f;
+	phones p;
 	Fin.open(pathFile);
 	friends ptr;
 	Fin.read((char*)&ptr, sizeof(friends));
 	ptr.printFriend();
+	Fin.close();
+	phones phonePtr;
+	Fin.read((char*)&phonePtr, sizeof(phones));
+	phonePtr.printPhones();
 	Fin.close();
 
 }
@@ -49,15 +66,15 @@ void delStringofFile() {
 int main() {
 	friends f;
 	phones tel;
-	
+	inputDatas();
 	std::string path = "friends.txt";
 	writeToFile(path, f, tel); //Here We should use structures like datas of dataBase 
-	/*path  = "phones.txt";
-	writeToFile(path, f, tel);*/
-	//path = "friends.txt";
+	path  = "phones.txt";
+	writeToFile(path, f, tel);
+	path = "friends.txt";
 	readFrFile(path, f, tel);
-	/*path = "phones.txt";
-	readFrFile(path, f, tel);*/
+	path = "phones.txt";
+	readFrFile(path, f, tel);
 
 	
 
